2e042127596613283339415a38df7587af873c6a
fedelerner88 <fedelerner88@gmail.com>
2014-10-25 04:26:32 +0000

    WebService AdministradorOrdenesDeDespacho -> Terminado Ya funciona y publica el WSDL... TESTEAR
    REST+JSON: Recepción Articulos a Despachar
    Lo implementé (no está funcionando), para mi tiene que
    estar en el proyecto WEB.. Pero estaría en el cliente
    y no en el servidor??

--- TPO-Despacho/ejbModule/tpo/despacho/ws/AdministradorOrdenDeDespachoBean.java
+++ TPO-Despacho/ejbModule/tpo/despacho/ws/AdministradorOrdenDeDespachoBean.java
@@ -2,10 +2,19 @@ package tpo.despacho.ws;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import javax.ejb.Stateless;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -14,8 +23,12 @@ import tpo.despacho.entidades.DetalleOrdenDeDespacho;
 import tpo.despacho.entidades.OrdenDeDespacho;
 import tpo.despacho.entidades.PortalWeb;
 import tpo.despacho.entidades.SolicitudDeArticulo;
+import tpo.despacho.vos.ArticuloVO;
 import tpo.despacho.vos.DetalleOrdenDeDespachoVO;
+import tpo.despacho.vos.FichaTecnicaVO;
 import tpo.despacho.vos.OrdenDeDespachoVO;
+
+// URL WSDL: http://localhost:8080/TPO-Despacho/AdministradorOrdenDeDespachoBean?wsdl
 
 @Stateless
 @WebService
@@ -59,12 +72,60 @@ public class AdministradorOrdenDeDespachoBean implements AdministradorOrdenDeDes
     			solicitudDeArticulo.setDetalleOrdenDeDespacho(dodd);
     			solicitudDeArticulo.setCantidadRestante(dodd.getCantidad());
     			
-    			// ENVIAR ASINCRONICAMENTE LA SOLICITUD DEL ARTICULO AL DEPOSITO CORRESPONDIENTE.
+    			dodd.setSolicitudDeArticulo(solicitudDeArticulo);
     		}
+    		
+			// ENVIAR ASINCRONICAMENTE LAS SOLICITUDES DE ARTICULO AL DEPOSITO CORRESPONDIENTE.
+    		enviarSolcitudesDeArticuloAsync(ordenDeDespacho);
     		
     		manager.persist(ordenDeDespacho);
     	}
     	
     	return false;
+	}
+    
+    private static void enviarSolcitudesDeArticuloAsync(OrdenDeDespacho ordenDeDespacho) {
+
+		final Properties env = new Properties();
+		  env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+		  env.put(Context.PROVIDER_URL, System.getProperty(Context.PROVIDER_URL, "remote://localhost:4447"));
+
+		  try {
+		
+		Context context = new InitialContext(env);
+			  
+		// buscar la Connection Factory en JNDI
+		String connectionFactoryString = System.getProperty("connection.factory", "jms/RemoteConnectionFactory");
+		ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup(connectionFactoryString);
+		// buscar la Cola en JNDI
+		String destinationString = System.getProperty("destination", "jms/queue/Articulos");
+		Destination  destination = (Destination) context.lookup(destinationString);
+		// crear la connection y la session a partir de la connection
+		Connection connection = connectionFactory.createConnection(System.getProperty("username", "test2"), System.getProperty("password", "test1234."));
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		connection.start();
+		// crear un producer para enviar mensajes usando la session
+		 MessageProducer producer = session.createProducer(destination);
+		 
+		 // POR CADA DETALLE DE LA ORDEN DE DESPACHO
+		 for(DetalleOrdenDeDespacho dodd : ordenDeDespacho.getDetallesOrdenDeDespacho())
+		 {
+			// Crear un mensaje de tipo Object Message y setearle el contenido
+			ObjectMessage message = session.createObjectMessage();
+			message.setObject(dodd.getSolicitudDeArticulo().getSolicitudDeArticuloVO());
+			
+			// Enviar el mensaje
+			producer.send(message);
+		 }		
+		
+		// TODO: recordar cerrar la session y la connection en un bloque “finally”
+		System.out.print("Envió el mensaje...");
+		connection.close();
+
+		} catch (Exception e) {
+			System.out.println("Error al efectuar pedido: " + e);
+			e.printStackTrace();
+		}
+
 	}
 }
