1b58e43655a37a75ca530bef0bf4c6934a3722f7
fedelerner88@gmail.com <fedelerner88@gmail.com@cce12da9-0425-9cca-f9fa-b12f9f67d222>
2014-10-27 02:12:48 +0000

    LogisticaYMonitoreo -> Para testear WS OrdenDeDespacho Todavía NO funciona.. Tira errores por todos lados!

---
+++ trunk/TPO-Test-LogisticaYMonitoreoCliente/src/tpo/logistica/cliente/LogisticaYMonitoreo.java
@@ -0,0 +1,45 @@
+package tpo.logistica.cliente;
+
+import java.util.List;
+
+import tpo.logistica.wscliente.*;
+
+public class LogisticaYMonitoreo {
+
+	public static void main(String[] args) {
+	        System.out.println("***********************");
+	        System.out.println("Create Web Service Client...");
+	        AdministradorOrdenDeDespachoBeanService service1 = new AdministradorOrdenDeDespachoBeanService();
+	        System.out.println("Create Web Service...");
+	        AdministradorOrdenDeDespachoBean port1 = service1.getAdministradorOrdenDeDespachoBeanPort();
+	        System.out.println("Call Web Service Operation...");
+	        
+	        // CREO UNA ORDEN DE DESPACHO
+	        OrdenDeDespachoVO odd = new OrdenDeDespachoVO();
+	        odd.setIdOrdenDeDespacho(1);
+	        odd.setNombrePortalWeb("PORTALWEB1");
+	        odd.setNombreLogisticaYMonitoreo("LOGISTICA1");
+	        
+	        List<DetalleOrdenDeDespachoVO> detalles = odd.getDetallesOrdenDeDespachoVO();
+	        DetalleOrdenDeDespachoVO dodd1 = new DetalleOrdenDeDespachoVO();
+	        dodd1.setCodigoArticulo(1351953);
+	        dodd1.setCantidad(2);
+	        detalles.add(dodd1);
+	        DetalleOrdenDeDespachoVO dodd2 = new DetalleOrdenDeDespachoVO();
+	        dodd2.setCodigoArticulo(1607594);
+	        dodd2.setCantidad(1);
+	        detalles.add(dodd2);
+	        
+	        System.out.println("Server said: " + port1.recepcionOrdenDeDespacho(odd));
+	        //Please input the parameters instead of 'null' for the upper method!
+	
+	        //System.out.println("Create Web Service...");
+	        //AdministradorOrdenDeDespachoBean port2 = service1.getAdministradorOrdenDeDespachoBeanPort();
+	        //System.out.println("Call Web Service Operation...");
+	        //System.out.println("Server said: " + port2.recepcionOrdenDeDespacho(null));
+	        //Please input the parameters instead of 'null' for the upper method!
+	
+	        System.out.println("***********************");
+	        System.out.println("Call Over!");
+	}
+}
