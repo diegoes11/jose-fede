8a862496906f2ef5158681dcea95a0eabf24307a
fedelerner88@gmail.com <fedelerner88@gmail.com@cce12da9-0425-9cca-f9fa-b12f9f67d222>
2014-11-02 20:52:01 +0000

    Lógica pasada a los SB Administrador

--- trunk/TPO-Despacho/ejbModule/tpo/despacho/mdbs/ArticulosMDB.java
+++ trunk/TPO-Despacho/ejbModule/tpo/despacho/mdbs/ArticulosMDB.java
@@ -1,18 +1,13 @@
 package tpo.despacho.mdbs;
 
-import java.util.List;
-
 import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
-import tpo.despacho.entidades.Articulo;
-import tpo.despacho.entidades.Deposito;
-import tpo.despacho.entidades.IdArticulo;
+import tpo.despacho.facade.DespachoFacade;
 import tpo.despacho.vos.ArticuloVO;
 
 @MessageDriven(
@@ -22,8 +17,8 @@ import tpo.despacho.vos.ArticuloVO;
 		})
 public class ArticulosMDB implements MessageListener {
 	
-	@PersistenceContext(unitName="DespachoBD")
-	private EntityManager manager;
+	@EJB
+	DespachoFacade despachoFacade;
 
     public ArticulosMDB() {
     }
@@ -33,33 +28,12 @@ public class ArticulosMDB implements MessageListener {
     	{
     		ObjectMessage objectMessage = (ObjectMessage)message;
     		ArticuloVO articuloVO = (ArticuloVO)objectMessage.getObject();
-    		// DAR DE ALTA EL ARTICULO EN NUESTRO SISTEMA
-    		Articulo articulo = buscarArticulo(articuloVO.getCodigo());
-    		Deposito deposito = buscarDeposito(articuloVO.getDeposito());
-    		if(articulo == null && deposito != null){
-    			articulo = new Articulo();
-    			articulo.setArticuloVO(articuloVO);
-    			articulo.setId(new IdArticulo(articuloVO.getCodigo(), deposito));
-    			manager.persist(articulo);
-    		}
+    		despachoFacade.altaArticulo(articuloVO);
     	}
     	catch (Exception e)
     	{
     		System.out.println(e.getStackTrace());
     	}
     }
-    
-    @SuppressWarnings("unchecked")
-	private Articulo buscarArticulo(int codigo){
-    	List<Articulo> articulos = manager.createQuery("SELECT a FROM Articulo a WHERE a.id.codigo =:c").setParameter("c", codigo).getResultList();
-    	if(articulos.size() == 0) {
-    		return null;
-    	}
-    	return articulos.get(0);
-    }
-    
-    private Deposito buscarDeposito(String nombre){
-    	Deposito deposito = manager.find(Deposito.class, nombre);
-    	return deposito;
-    }
+	
 }
--- trunk/TPO-Despacho/ejbModule/tpo/despacho/sessions/AdministradorArticulosBean.java
+++ trunk/TPO-Despacho/ejbModule/tpo/despacho/sessions/AdministradorArticulosBean.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import tpo.despacho.entidades.Articulo;
+import tpo.despacho.entidades.Deposito;
+import tpo.despacho.entidades.IdArticulo;
 import tpo.despacho.entidades.SolicitudDeArticulo;
 import tpo.despacho.vos.ArticuloVO;
 
@@ -23,13 +25,30 @@ public class AdministradorArticulosBean implements AdministradorArticulos {
     	
     }
     
+    // Métodos auxiliares
     private SolicitudDeArticulo buscarSolicitudDeArticulo(int idSolicitudDeArticulo){
     	SolicitudDeArticulo solicitudDeArticulo = manager.find(SolicitudDeArticulo.class, idSolicitudDeArticulo);
     	if(solicitudDeArticulo != null)
     		return solicitudDeArticulo;
     	return null;
     }
+    
+    @SuppressWarnings("unchecked")
+	private Articulo buscarArticulo(int codigo){
+    	List<Articulo> articulos = manager.createQuery("SELECT a FROM Articulo a WHERE a.id.codigo =:c")
+    								.setParameter("c", codigo).getResultList();
+    	if(articulos.size() == 0) {
+    		return null;
+    	}
+    	return articulos.get(0);
+    }
+    
+    private Deposito buscarDeposito(String nombre){
+    	Deposito deposito = manager.find(Deposito.class, nombre);
+    	return deposito;
+    }
 
+    // Métodos
 	public boolean recepcionDeArticulos(int idSolicitudDeArticulo, int cantidad) {
 		SolicitudDeArticulo solicitudDeArticulo = buscarSolicitudDeArticulo(idSolicitudDeArticulo);
 		if(solicitudDeArticulo != null){
@@ -49,5 +68,25 @@ public class AdministradorArticulosBean implements AdministradorArticulos {
     		articulosVO.add(articuloVO);
     	}
     	return articulosVO;
+	}
+	
+	public boolean altaArticulo(ArticuloVO articuloVO){
+		try{
+			Articulo articulo = buscarArticulo(articuloVO.getCodigo());
+			Deposito deposito = buscarDeposito(articuloVO.getDeposito());
+			if(articulo == null && deposito != null){
+				articulo = new Articulo();
+				articulo.setArticuloVO(articuloVO);
+				articulo.setId(new IdArticulo(articuloVO.getCodigo(), deposito));
+				manager.persist(articulo);
+				return true;
+			}
+			return false;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		
 	}
 }
